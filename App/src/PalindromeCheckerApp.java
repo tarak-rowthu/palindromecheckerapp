7b14e52c83e4205da4f84c63efc8166652b206b8
unknown <taraknagavenkatadurgeshrowthu@gmail.com>
2026-02-18 16:17:35 +0530

    Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,22 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome back to Palindrome checkerapp management system");
-    }
-}
+
+
+        String input = "madam";
+                boolean isPalindrome = true;
+
+
+                for (int i = 0; i < input.length() / 2; i++) {
+                    if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                if (isPalindrome) {
+                    System.out.println(input + " is a palindrome");
+                } else {
+                    System.out.println(input + " is not a palindrome");
+                }
+            }
+        }
