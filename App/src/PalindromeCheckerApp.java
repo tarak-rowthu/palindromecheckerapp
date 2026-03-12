a3e2490da3a9c433ec14666a8ba7863576c551b1
Rudhira370 <taraknagavenkatadurgeshrowthu@gmail.com>
2026-03-12 14:13:17 +0530

    Object-Oriented Palindrome Service

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -5,28 +5,38 @@ import java.util.Deque;
 import java.util.LinkedList;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
                 System.out.print("Input: ");
                 String input = sc.nextLine();
 
-                // Normalize string: remove spaces and convert to lowercase
-                String normalized = input.replaceAll("\\s+", "").toLowerCase();
+                PalindromeService service = new PalindromeService();
 
-                boolean isPalindrome = true;
+                boolean result = service.checkPalindrome(input);
 
-                // Compare characters from both ends
-                for (int i = 0; i < normalized.length() / 2; i++) {
-
-                    if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                        isPalindrome = false;
-                        break;
-                    }
-                }
-
-                System.out.println("Is Palindrome? : " + isPalindrome);
+                System.out.println("Is Palindrome? : " + result);
 
                 sc.close();
+            }
+        }
+
+        class PalindromeService {
+
+            public boolean checkPalindrome(String input) {
+
+                int start = 0;
+                int end = input.length() - 1;
+
+                while (start < end) {
+
+                    if (input.charAt(start) != input.charAt(end)) {
+                        return false;
+                    }
+
+                    start++;
+                    end--;
+                }
+
+                return true;
             }
         }
